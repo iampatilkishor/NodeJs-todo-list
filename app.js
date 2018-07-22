1813a988abfbd34d11a84ac0c72cc8e0c7ee60cd
kishorcse <saikishor.patil@gmail.com>
2018-07-22 23:52:51 +0530

    push new changes to heroku

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 var express = require('express');
-var cors = require('cors')
+var cors = require('cors');
+const PORT = process.env.PORT || 5000
 
-var todoController = require('./controller/todoController');
-var app = express();
-app.use(express.static(path.join(__dirname, 'public')))
-todoController(app);
-app.get('/', function(req, res){
-    res.sendFile('public/index.html') 
-});
-app.listen(8080);
-console.log('You are listening to port 3000');
+// var todoController = require('./controller/todoController');
+
+
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => res.sendFile('public/index.html'))
+  .listen(PORT);
+
+
+
+
+// var todoController = require('./controller/todoController');
+// var app = express();
+// app.use(express.static(path.join(__dirname, 'public')))
+// todoController(app);
+// app.get('/', function(req, res){
+//     res.sendFile('public/index.html') 
+// });
+// app.listen(8080);
+// console.log('You are listening to port 3000');
